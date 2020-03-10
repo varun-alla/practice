24144fafadc40777f1ffb3526778e4f295985e92
varun-alla <alla.varunsai@gmail.com>
2020-03-10 19:32:18 +0530

    fourthcommit

--- week1/question2.java
+++ week1/question2.java
@@ -10,25 +10,37 @@ class question2
 		for(int i=0;i<input1.length;i++)
             System.out.println(minFlip(input1[i],input2[i],input3[i])+" -->"+expectedout[i]);
 	}
+	static String zeros(String s,int n){
+	n=n-s.length();
+	while(n-->0){
+		s="0"+s;
+	}
+	return s;
+	}
+	static int max(int a,int b,int c){
+	return a>b?(a>c?(a):(c)):(c>b?(c):(b));
+	}
 	static int minFlip(int a,int b,int c ){
+		String b1 = Integer.toBinaryString(a);//O(n)
+		String b2 = Integer.toBinaryString(b);
+		String b3 = Integer.toBinaryString(c);
+		int n = max(b1.length(),b2.length(),b3.length());
+		b1 = zeros(b1,n);
+		b2 = zeros(b2,n);
+		b3 = zeros(b3,n);//O(n);
 		int count=0;
-		while(a>0&&b>0&&c>0){
-			int x = c&1;
-			int y = a&1;
-			int z = b&1;
-			a<<=1;
-			b<<=1;
-			c<<=1;
+		for(int i=0;i<n;i++){
+			char x = b3.charAt(i);
 			switch(x){
-			case 0: if(y==1&&z==1){
+			case '0': if(b1.charAt(i)=='1'&&b2.charAt(i)=='1'){
 								count = count+2;
-								}else if(y==1&&z==0){
+								}else if(b1.charAt(i)=='1'&&b2.charAt(i)=='0'){
 									count++;
-								}else if(y==0&&z==1){
+								}else if(b1.charAt(i)=='0'&&b2.charAt(i)=='1'){
 									count++;
 									}break;
-				
-			case 1: if(y==0&&z==0){
+
+			case '1': if(b1.charAt(i)=='0'&&b2.charAt(i)=='0'){
 									count++;
 								}break;
 			}
